3a3bb9fc2ec72eef74754ed467e8b9d955907cd7
Mafa <117449106+mafampofu@users.noreply.github.com>
2023-09-03 23:37:43 +0200

    Update urls.py

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
+    path('<int:course_id>/submit/', views.submit, name="submit"),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
     # ex: /onlinecourse/5/
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
     # ex: /enroll/5/
